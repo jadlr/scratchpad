262bf3296fa78a2b4d57b4e9404a397ed7b89d68
Jonas Adler <jonas@toyrocketscience.com>
2019-09-30 15:40:42 +0200

    Add scalafmt config

--- src/main/scala/io/trsc/scratchpad/Main.scala
+++ src/main/scala/io/trsc/scratchpad/Main.scala
@@ -1,9 +1,8 @@
 package io.trsc.scratchpad
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ ExitCode, IO, IOApp }
 
 object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = {
+  override def run(args: List[String]): IO[ExitCode] =
     IO.delay(println("Hello")).map(_ => ExitCode.Success)
-  }
 }
