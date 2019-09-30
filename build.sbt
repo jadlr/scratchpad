da6db127e1bb0166b7d9e9f4d3098a85f35e1609
Jonas Adler <jonas@toyrocketscience.com>
2019-09-30 16:25:26 +0200

    Improve project config

--- build.sbt
+++ build.sbt
@@ -1,22 +1,23 @@
 import Dependencies._
 
-lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
+lazy val root = (project in file(".")).settings(
+  inThisBuild(
+    List(
       organization := "io.trsc",
       scalaVersion := "2.12.10",
-      version      := "0.1.0-SNAPSHOT",
+      version := "0.1.0-SNAPSHOT",
       scalacOptions ++= ScalacOptions.opts,
-      scalacOptions in (Compile, console) ~= (_.filterNot(ScalacOptions.excludeInConsoleAndCompile)),
+      scalacOptions in (Compile, console) --= ScalacOptions.excludeInConsoleAndCompile,
       addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3"),
       shellPrompt := { state ⇒
-        s"[${scala.Console.CYAN}%s${scala.Console.RESET}] λ " format {
-          Project.extract(state).getOpt(sbt.Keys.name) getOrElse {
-            Project.extract(state).currentProject.id
+          s"[${scala.Console.CYAN}%s${scala.Console.RESET}] λ ".format {
+            Project.extract(state).getOpt(sbt.Keys.name).getOrElse {
+              Project.extract(state).currentProject.id
+            }
           }
         }
-      }
-    )),
-    name := "scratchpad",
-    libraryDependencies ++= runtimeDeps ++ testDeps
-  )
+    )
+  ),
+  name := "scratchpad",
+  libraryDependencies ++= runtimeDeps ++ testDeps
+)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,24 +2,21 @@ import sbt._
 
 object Dependencies {
 
+  lazy val runtimeDeps: Seq[ModuleID] = Seq(
+    "org.typelevel"  %% "cats-core"      % CatsVersion,
+    "org.typelevel"  %% "cats-effect"    % CatsEffectVersion,
+    "io.circe"       %% "circe-core"     % CirceVersion,
+    "io.circe"       %% "circe-generic"  % CirceVersion,
+    "io.circe"       %% "circe-parser"   % CirceVersion,
+    "ch.qos.logback" % "logback-classic" % LogbackVersion
+  )
+  lazy val testDeps: Seq[ModuleID] = Seq(
+    "org.specs2" %% "specs2-core" % Specs2Version
+  ).map(_ % Test)
+
   private val CatsVersion       = "2.0.0"
   private val CatsEffectVersion = "2.0.0"
   private val CirceVersion      = "0.12.1"
   private val LogbackVersion    = "1.2.3"
   private val Specs2Version     = "4.7.1"
-
-
-  lazy val runtimeDeps: Seq[ModuleID] = Seq(
-    "org.typelevel"         %% "cats-core"              % CatsVersion,
-    "org.typelevel"         %% "cats-effect"            % CatsEffectVersion,
-    "io.circe"              %% "circe-core"             % CirceVersion,
-    "io.circe"              %% "circe-generic"          % CirceVersion,
-    "io.circe"              %% "circe-parser"           % CirceVersion,
-    "ch.qos.logback"        %  "logback-classic"        % LogbackVersion
-  )
-
-  lazy val testDeps: Seq[ModuleID] = Seq(
-    "org.specs2" %% "specs2-core" % Specs2Version
-  ).map(_ % Test)
-
 }
